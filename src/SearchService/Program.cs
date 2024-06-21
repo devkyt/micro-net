7da9e41d8be7f68865e660b1b52f28b1f27d15cb
Kyrylo Tykhanskyi <ktykhanskyi@gmail.com>
2024-06-21 16:58:23 +0200

    add MongoDb Init setup

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -13,4 +13,13 @@ app.UseAuthorization();
 app.MapControllers();
 
 
+await DB.InitAsync("SearchDB", MongoClientSettings
+    .FromConnectionString(builder.Configuration.GetConnectionString("MongoDBConnection")));
+
+await DB.Index<Item>()
+    .Key(x => x.Type, KeyType.Text)
+    .Key(x => x.Model, KeyType.Text)
+    .Key(x => x.Condition, KeyType.Text)
+    .CreateAsync();
+
 app.Run();
